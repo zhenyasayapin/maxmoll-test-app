564f34ca3d0ce29d730a90865e1a52bbd7d87b1b
Zhenya Sayapin <ep.sayapin@gmail.com>
2025-08-04 15:22:21 +0400

    Add search filter for Order entity and implement test for filtered orders

--- src/src/Entity/Order.php
+++ src/src/Entity/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use App\Repository\OrderRepository;
@@ -14,6 +16,7 @@ use Symfony\Component\Serializer\Attribute\Groups;
     operations: [new GetCollection()],
     normalizationContext: ['groups' => ['order:read']],
 )]
+#[ApiFilter(SearchFilter::class, properties: ['status' => 'exact'])]
 class Order
 {
     #[ORM\Id]
--- src/tests/OrderTest.php
+++ src/tests/OrderTest.php
@@ -4,6 +4,7 @@ namespace App\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\Order;
+use App\Enum\OrderStatusEnum;
 use App\Factory\OrderFactory;
 use App\Serializer\OrderDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
@@ -44,6 +45,39 @@ class OrderTest extends ApiTestCase
             $this->assertNotEmpty($order->getCreatedAt());
             $this->assertNotEmpty($order->getWarehouse());
             $this->assertNotEmpty($order->getStatus());
+        }
+    }
+
+    public function testGetFilteredOrders(): void
+    {
+        OrderFactory::createMany(10, function () {
+            return [
+                'status' => OrderStatusEnum::ACTIVE->value,
+            ];
+        });
+
+        OrderFactory::createMany(10, function () {
+            return [
+                'status' => OrderStatusEnum::CANCELLED->value,
+            ];
+        });
+
+        $response = static::createClient()->request('GET', '/api/orders?status=active');
+
+        $serializer = new Serializer([
+            new OrderDenormalizer(),
+            new ArrayDenormalizer(),
+        ]);
+
+        $orders = $serializer->denormalize(
+            json_decode($response->getContent(), true)['member'] ?? [],
+            Order::class . '[]',
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        foreach ($orders as $order) {
+            $this->assertEquals(OrderStatusEnum::ACTIVE->value, $order->getStatus());
         }
     }
 }
